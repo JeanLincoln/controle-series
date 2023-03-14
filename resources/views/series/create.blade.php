f865574094ed116350e3670d567ddb754fd9e0f1
Lincoln <jeanlincol@gmail.com>
2023-03-13 21:13:45 -0300

    Criacao de um projeto iniciante de laravel, MVC criados, laravel-mix adicionado.

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,6 @@
+<x-layout title="Nova Série">
+     <form action="" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" placeholder="Digite o nome da série">
+     </form>
+</x-layout>
