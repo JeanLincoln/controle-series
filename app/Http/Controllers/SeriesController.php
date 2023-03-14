89416ca5377227e2619ad73e8d59f36936286bf0
Lincoln <jeanlincol@gmail.com>
2023-03-14 16:11:27 -0300

    criacao de serie adicionada ao projeto, DB FACADES utilizado

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Serie;
 
 class SeriesController extends Controller
 {
     public function index (Request $request)
     {
-        $series = ['Punisher',
-        'Lost',
-        'Grey\'s Anatomy'];
+        $series = Serie::all();
 
         return view('series.index', compact('series'));
     }
@@ -18,5 +19,14 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+        return redirect ('/series');
     }
 }
