4cee928010a2947cdbcc4d7a7933a7f0d35492cb
Lincoln <jeanlincol@gmail.com>
2023-03-27 13:14:27 -0300

    Definido escopo global para series

--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,5 +17,12 @@ class Serie extends Model
     public function temporadas()
     {
         return $this->hasMany(Season::class, 'series_id');
+    }
+
+    protected static function booted()
+    {
+        self::addGlobalScope('ordered',function(Builder $queryBuilder){
+            $queryBuilder->orderBy('nome');
+        });
     }
 }
