7f2b04297ce6bf2afb4c137be481c05174485c05
Lincoln <jeanlincol@gmail.com>
2023-03-30 21:22:21 -0300

    Utilizando repositorios para lidar com transacoes de BD

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -9,10 +9,17 @@ use App\Models\Series;
 use App\Models\Season;
 use App\Models\Episode;
 use App\Http\Requests\SeriesFormRequest;
+use App\Repositories\SeriesRepository;
 
 
 class SeriesController extends Controller
 {
+
+    public function __construct(private SeriesRepository $repository)
+    {
+
+    }
+
     public function index (Request $request)
     {
         $series = Series::all();
@@ -29,33 +36,7 @@ class SeriesController extends Controller
     public function store(SeriesFormRequest $request)
     {
         
-        DB::beginTransaction();
-
-            $serie = Series::create($request->all());
-
-            $seasons = [];
-    
-            for($i=1;$i <= $request->seasonsQty; $i++){
-                $seasons[] = [
-                    'series_id' => $serie->id,
-                    'number' => $i
-                ];
-            };
-    
-            Season::insert($seasons);
-    
-            $episodes = [];
-            foreach($serie->seasons as $season)
-                for($j=1; $j <= $request->episodesPerSeason; $j++){
-                    $episodes[] =[
-                        'season_id' => $season->id,
-                        'number' => $j
-                    ];
-            };
-    
-            Episode::insert($episodes);
-        
-            DB::commit();
+        $serie = $this->repository->add($request);
 
         return to_route('series.index')
             ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso!");
